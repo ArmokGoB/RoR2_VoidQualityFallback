4d347f7c026c0995e7bcdf0dfb3bebf0de399e02
ArmokGoB <alexairobert@hotmail.com>
2026-05-06 04:32:58 -0700

    Added compatibility functions

--- VoidQualityFallback/Compatibility/QualityCompatibility.cs
+++ VoidQualityFallback/Compatibility/QualityCompatibility.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ItemQualities;
+﻿using ItemQualities;
 using RoR2;
 
 namespace VoidQualityFallback.Compatibility
 {
     internal static class QualityCompatibility
     {
+        public static bool IsQualityItem(ItemIndex item)
+        {
+            if (GetQuality(item) == QualityTier.None)
+                return false;
+
+            return true;
+        }
+        public static QualityTier GetQuality(ItemIndex item)
+        {
+            return QualityCatalog.GetQualityTier(item);
+        }
+        public static ItemQualityGroup GetQualityGroup(ItemQualityGroupIndex item)
+        {
+            if (item == ItemQualityGroupIndex.Invalid)
+                return null;
+
+            return QualityCatalog.GetItemQualityGroup(item);
+        }
+        public static ItemIndex GetItem(ItemIndex item)
+        {
+            if (item == ItemIndex.None)
+                return ItemIndex.None;
+
+            return QualityCatalog.GetItemIndexOfQuality(item, QualityTier.None);
+        }
+        public static ItemIndex GetItem(ItemIndex item, QualityTier tier)
+        {
+            if (item == ItemIndex.None)
+                return ItemIndex.None;
+
+            return QualityCatalog.GetItemIndexOfQuality(item, tier);
+        }
+        public static bool HasQualityVariant(ItemIndex item)
+        {
+            if (GetItem(item, QualityTier.Uncommon) == ItemIndex.None)
+                return false;
+
+            return true;
+        }
     }
 }
